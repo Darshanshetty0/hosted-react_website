c877b30f12ff39a99867717778660badf008ed1a
Darshan Shetty <darshanshetty253101@gmail.com>
2022-09-30 04:36:14 +0530

    updated files like Billing, Business, Button, CardDeal, Clients, Stats, Feedback, Stats, Testimonials under components

--- hosted-react_website/src/components/Billing.jsx
+++ hosted-react_website/src/components/Billing.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { apple, bill, google} from '../assets';
+import styles, {layout} from '../style';
 
-const Billing = () => {
-  return (
-    <div>Billing</div>
-  )
-}
+const Billing = () => (
+  <section id='product' className={layout.sectionImgReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img src={bill} alt="billing" className='w-[100%] h-[100%] relative z-[5]'/>
+      <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient'/>
+      <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient'/>
+    </div>
+
+    <div className={`mr-1`}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}> Easily control your <br className='sm:block hidden'/> billing & invoicing. </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          This can be done using our high end features that you can discover in the premium section of our app.
+        </p>
+
+        <div className='flex flex-row flex-wrap sm:mt-10 mt-6'>
+          <img src={apple} alt="apple_store" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer'/>
+          <img src={google} alt="google_play" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer'/>
+        </div>
+      </div>
+    </div>
+  </section>
+)
+  
+
 
 export default Billing
--- hosted-react_website/src/components/CardDeal.jsx
+++ hosted-react_website/src/components/CardDeal.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { card } from "../assets";
+import styles, {layout} from "../style";
+import Button from './Button';
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+  <div className={layout.sectionInfo}>
+    <h2 className={styles.heading2}>
+      Find a better card deal <br className="sm:block"/> in a few easy steps.
+    </h2>
+    <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Find deals for every card that you own and get exclusive offers from multiple brands across the internet. Use these deals to cashback rewards and much more.
+    </p>
+
+    <Button styles="mt-10 rounded-full"/>
+  </div>
+
+  <div className={layout.sectionImg}>
+    <img src={card} alt="card" className="w-[100%] h-[100%]"/>
+  </div>
+</section>
+)
 
 export default CardDeal
